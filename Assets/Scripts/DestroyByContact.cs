12e6f83f0be0ed3eb80755f8c2ca4bf43f477ffb
LeahCim <bogus739-github@yahoo.de>
2014-05-26 21:52:01 +0100

    Add explosion prefab and logic to Asteroid

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,11 +3,15 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour {
 
+	public GameObject explosion;
+
 	void OnTriggerEnter(Collider other) {
 		if(other.tag == "Boundary")
 		{
 			return;
 		}
+
+		Instantiate(explosion, transform.position, transform.rotation);
 		Destroy(other.gameObject);
 		Destroy (gameObject);
 	}
