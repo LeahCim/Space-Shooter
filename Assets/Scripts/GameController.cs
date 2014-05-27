fc77031d06357d645dd1a204525ddd66c8198011
LeahCim <bogus739-github@yahoo.de>
2014-05-27 20:24:41 +0100

    Create a wave of hazards with delays between

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,22 +5,31 @@ public class GameController : MonoBehaviour {
 
 	public GameObject hazard;
 	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
 
 	void Start()
 	{
-		SpawnWaves();
+		StartCoroutine ( SpawnWaves() );
 	}
 
-	void SpawnWaves()
+	IEnumerator SpawnWaves ()
 	{
-		Vector3 spawnPosition = new Vector3
-			(
-				Random.Range(-spawnValues.x, spawnValues.x),
-				spawnValues.y,
-				spawnValues.z
-			);
-		Quaternion spawnRotation = Quaternion.identity;
+		yield return new WaitForSeconds(startWait);
 
-		Instantiate(hazard, spawnPosition, spawnRotation);
+		for(int i = 0; i < hazardCount; i++)
+		{
+			Vector3 spawnPosition = new Vector3
+				(
+					Random.Range(-spawnValues.x, spawnValues.x),
+					spawnValues.y,
+					spawnValues.z
+				);
+			Quaternion spawnRotation = Quaternion.identity;
+
+			Instantiate(hazard, spawnPosition, spawnRotation);
+			yield return new WaitForSeconds(spawnWait);
+		}
 	}
 }
