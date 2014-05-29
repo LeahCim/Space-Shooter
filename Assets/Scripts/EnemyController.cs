18ab91de7939d248a8ca5ddbc81b29a16c7723ef
LeahCim <bogus739-github@yahoo.de>
2014-05-29 21:26:57 +0100

    Make Enemy follow Player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour
 {
+	public GameObject player;
+	public float speed;
 
 	// Use this for initialization
 	void Start ()
@@ -13,6 +15,13 @@ public class EnemyController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+		Vector3 heading = player.transform.position - transform.position;
 
+		rigidbody.velocity = new Vector3
+		(
+			heading.normalized.x * speed,
+			rigidbody.velocity.y,
+			rigidbody.velocity.z
+		);
 	}
 }
