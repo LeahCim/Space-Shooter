6cf8aeca83010d123112797fb9559aae6983a6a9
LeahCim <bogus739-github@yahoo.de>
2014-05-30 20:39:52 +0100

    Fix Enemy prefabs following Player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,31 +3,39 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour
 {
-	public GameObject player;
 	public ProximitySensor proximitySensor;
 	public float speed;
 	public Boundary boundary;
 	public float tilt;
 	public float randomMoveDelay;
-	private float nextRandomMove = 0;
 
 	public GameObject shot;
 	public Transform shotSpawn;
 	public float fireRate;
 
+	private GameObject player;
+	private float nextRandomMove = 0;
 	private float nextFire = 0.0F;
+
+	void Start()
+	{
+		player = GameObject.FindWithTag("Player");
+		if (player == null)
+		{
+			Debug.Log("Cannot find the 'Player' object");
+		}
+	}
 
 	void Update ()
 	{
-		if (Time.time > nextFire &&
-		    transform.position.z < boundary.zMax - 5)
+		if (Time.time > nextFire && transform.position.z < boundary.zMax - 5)
 		{
 			nextFire = Time.time + fireRate;
 			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
 			audio.Play();
 		}
 	}
-	
+
 	void FixedUpdate ()
 	{
 		if(proximitySensor.triggered) {
@@ -50,8 +58,8 @@ public class EnemyController : MonoBehaviour
 				Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
 				0,
 				Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
-				);
-		
+			);
+
 		rigidbody.rotation = Quaternion.Euler(0, 180, rigidbody.velocity.x * -tilt);
 	}
 
@@ -59,15 +67,15 @@ public class EnemyController : MonoBehaviour
 	{
 		if(player != null) {
 			Vector3 heading = player.transform.position - transform.position;
-			
+
 			if(Mathf.Abs(heading.z) > 5.0)
 			{
 				rigidbody.velocity = new Vector3
-				(
-					heading.normalized.x * speed,
-					rigidbody.velocity.y,
-					rigidbody.velocity.z
-				);
+					(
+						heading.normalized.x * speed,
+						rigidbody.velocity.y,
+						rigidbody.velocity.z
+					);
 			}
 		}
 	}
